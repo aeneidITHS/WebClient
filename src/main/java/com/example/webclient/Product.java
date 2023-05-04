db19b779b54b3a329a3c38e82db01ff92c2647a7
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2023-05-04 21:35:04 +0200

    added json stuff

---
+++ src/main/java/com/example/webclient/Product.java
@@ -0,0 +1,48 @@
+package com.example.webclient;
+
+import javax.annotation.processing.Generated;
+
+@Generated("jsonschema2pojo")
+public class Product {
+
+
+
+        private Integer id;
+        private String name;
+        private String category;
+        private Double price;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+
+
+}
